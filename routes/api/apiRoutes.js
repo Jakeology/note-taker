67c1fed670af76c2f81ce77a4c9424c1f3a52a83
Jake <xxjakexx14@gmail.com>
2021-11-04 10:42:45 -0400

    added api route to add note via post

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -6,4 +6,16 @@ router.get("/notes", (req, res) => {
   return res.json(notes);
 });
 
+router.post("/notes", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = uuidv4();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNote(req.body, notes);
+    res.json(note);
+  }
+});
+
 module.exports = router;
