dd2f1cce6bd643eb538c4d30f5551b40e9aa2f85
Jake <xxjakexx14@gmail.com>
2021-11-04 10:52:44 -0400

    added all html routes to point to correct html page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const apiRoutes = require('./routes/api/apiRoutes');
+const htmlRoutes = require('./routes/html/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
