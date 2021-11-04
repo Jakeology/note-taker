92bb9c4104fe98a868b73ada6c6e35c7d672672d
Jake <xxjakexx14@gmail.com>
2021-11-04 10:31:10 -0400

    added api routes to show all notes at /api/notes

---
+++ routes/api/apiRoutes.js
@@ -0,0 +1,10 @@
+const { notes } = require("../../db/db");
+
+const router = require("express").Router();
+
+router.get("/notes", (req, res) => {
+  return res.json(notes);
+});
+
+module.exports  = router;
+
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const apiRoutes = require('./routes/api/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
